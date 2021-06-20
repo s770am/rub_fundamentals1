5055eaef2b5da9075e4c00d79fe2516b623c3fc7
s770am <s770am@gmail.com>
2021-06-20 15:52:18 -0500

    exercise2

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+#1
+# cost = 55.0
+# tip = cost % 0.2
+# puts tip
+
+#2
+# puts "he was " + 7.to_s + " years old"
+
+#3
+# puts " 45628 multiplied by 7839 is #{ 45628 * 7839} done like a pro"
+
+#4
+puts (10 < 20 && 30 < 20) || !(10 == 11)
